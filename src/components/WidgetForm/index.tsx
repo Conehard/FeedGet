bc5e9d28cbb93895f056ce3b25bc9623b174d69b
andre <andre@egend.com.br>
2022-05-04 13:13:12 -0300

    first commit

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,67 @@
+import {useState} from "react";
+import {CloseButton} from "../CloseButton";
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import {FeedbackTypeStep} from "./Steps/FeedbackTypeStep";
+import {FeedbackContentStep} from "./Steps/FeedbackContentStep";
+import {FeedbackSuccessStep} from "./Steps/FeedbackSuccessStep";
+
+export const feedbackTypes = {
+    BUG: {
+        title: "Problema",
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title: "Ideia",
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: "Outro",
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        }
+    },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+    const [feedbackSent, setFeedbackSent] = useState(false);
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+        setFeedbackSent(false);
+    }
+
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+            {feedbackSent ?
+                ( <FeedbackSuccessStep onFeebackRestartRequested={handleRestartFeedback} /> )
+                :
+                (<>
+                {!feedbackType ?
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
+                    :
+                    <FeedbackContentStep onFeedbackSent={() => setFeedbackSent(true)} onFeedbackRestartRequested={handleRestartFeedback} feedbackType={feedbackType}/>
+                }
+            </>)}
+
+
+            <footer className="text-xs text-neutral-400">
+                Feito com <span className="text-red-400">♥</span> por <a className="underline underline-offset-2"
+                                                                         href="https://github.com/conehard"
+                                                                         target="_blank">conehard</a>
+            </footer>
+        </div>
+    );
+}
